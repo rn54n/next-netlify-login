d12c14e4b127469b580da5a2fd2831fd1dfde794
D. Aaren San <denis.san@swissdentalsolutions.com>
2021-05-19 16:54:10 +0200

    test: add data read from user logged in

---
+++ functions/guides.js
@@ -0,0 +1,25 @@
+exports.handler = async (event, context) => {
+  // the context of the request
+  // determine if the user is logged in
+  const guides = [
+    { title: "asdlfasd fljaksdf lasjfd asdlkfjasdf", author: "bong" },
+    { title: "asdlfasd fljaksdf lasjfd asdlkfjasdf", author: "dong" },
+    { title: "asdlfasd fljaksdf lasjfd asdlkfjasdf", author: "cun" },
+  ];
+
+  // if browser makes request from user logged in
+  // get access to user
+  if (context.clientContext.user) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(guides),
+    };
+  }
+
+  return {
+    statusCode: 401,
+    body: JSON.stringify({
+      msg: "ah, ah, ah you must be logged in to see this!",
+    }),
+  };
+};
