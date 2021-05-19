92d43baa814fff716ee69c92a76bf40296e4177a
D. Aaren San <denis.san@swissdentalsolutions.com>
2021-05-19 15:27:44 +0200

    fix: fix superhero function netlify

--- functions/superhero.js
+++ functions/superhero.js
@@ -1,10 +1,10 @@
 exports.handler = async () => {
   console.log("function run");
-  const data = {name: 'John', age: 35, job: 'bouncer'}
+  const data = { name: "John", age: 35, job: "bouncer" };
 
   // return response to browser
   return {
     statusCode: 200,
-    body: JSON.stringify(data);
-  }
+    body: JSON.stringify(data),
+  };
 };
