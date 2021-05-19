9c1e1e0fc0e5beb407e5931523abfbad64958797
D. Aaren San <denis.san@swissdentalsolutions.com>
2021-05-19 14:39:01 +0200

    feat: add navbar login conditional

--- components/Navbar.js
+++ components/Navbar.js
@@ -23,12 +23,17 @@ export default function Navbar() {
               <a>Guides</a>
             </Link>
           </li>
-          <li onClick={login} className="btn">
-            Login/Signup
-          </li>
-          <li onClick={logout} className="btn">
-            Logout
-          </li>
+          {!user && (
+            <li onClick={login} className="btn">
+              Login/Signup
+            </li>
+          )}
+          {user && <li>{user.email}</li>}
+          {user && (
+            <li onClick={logout} className="btn">
+              Logout
+            </li>
+          )}
         </ul>
       </nav>
       <div className="banner">
